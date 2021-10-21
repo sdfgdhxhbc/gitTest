f973f0cde6ba5644b76721800d16f3e9b3f6d349
Tom <tom@qq.com>
2021-10-21 15:25:26 +0800

    first commit

---
+++ src/main/java/com/atguigu/javas.java
@@ -0,0 +1,12 @@
+package com.atguigu;
+
+/**
+ * @author: 17916
+ * @date: 2021/10/21 15:19
+ * @description:
+ */
+public class javas {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
