0fd819baa76b350a197bc7d0241028010086302b
Tom <tom@qq.com>
2021-10-21 15:33:13 +0800

    second commit

--- src/main/java/com/atguigu/javas.java
+++ src/main/java/com/atguigu/javas.java
@@ -8,5 +8,6 @@ package com.atguigu;
 public class javas {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git2");
     }
 }
